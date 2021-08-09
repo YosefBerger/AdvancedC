f5117fa7861fd09e7fc7d29dba376e43304d1e00
YosefBerger <yosefnberger@gmail.com>
2021-08-09 17:22:31 +0300

    Removed PauseTerminal and fixed auto stopping

--- AdvancedC/HelloWorld.c
+++ AdvancedC/HelloWorld.c
@@ -1,16 +1,8 @@
 #include <stdio.h>
 
-void PauseTerminal();
-
 int main()
 {
     printf("Hello, world!\n");
-    PauseTerminal();
-    return 0;
-}
 
-void PauseTerminal()
-{
-    printf("Press the enter key to continue...");
-    getchar();
+    return 0;
 }
